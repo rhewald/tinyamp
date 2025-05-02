44fad62ecccb4a163cefd34584b4b4d07af1b846
rhewald <3038833+rhewald@users.noreply.github.com>
2025-05-01 22:42:45 -0700

    Update eventspage.js

--- client/src/pages/eventspage.js
+++ client/src/pages/eventspage.js
@@ -5,7 +5,7 @@ function EventsPage() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/events')
+    fetch('https://tinyamp.onrender.com/api/events')
       .then(res => res.json())
       .then(data => setEvents(data));
   }, []);
